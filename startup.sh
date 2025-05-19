0159e8f33bab4d8d46687b346be3ca7236a7b7a6
ziyin.lin <ziyin5030@gmail.com>
2025-05-19 16:37:31 +0800

    fix : update

--- startup.sh
+++ startup.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# 如果 output.tar.gz 還沒解壓，就解壓它
-if [ -f output.tar.gz ]; then
-    echo "Extracting output.tar.gz..."
-    tar -xzf output.tar.gz -C .
-fi
-
-# 啟動 FastAPI (或 Flask) App
+source antenv/bin/activate
 exec uvicorn main:app --host=0.0.0.0 --port=8000
